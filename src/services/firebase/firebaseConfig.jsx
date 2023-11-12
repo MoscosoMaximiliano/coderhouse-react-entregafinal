34b9b86df139025994254038928cd334cd2b51af
MoscosoMaximiliano <moscosomaximiliano@gmail.com>
2023-11-12 18:21:34 -0300

    remove useless firestore methods

--- src/services/firebase/firebaseConfig.jsx
+++ src/services/firebase/firebaseConfig.jsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {doc, getDocs, getDoc, setDoc, collection, query, where, getFirestore, addDoc } from 'firebase/firestore' 
+import {doc, getDocs, getDoc, collection, query, where, getFirestore, addDoc } from 'firebase/firestore' 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
